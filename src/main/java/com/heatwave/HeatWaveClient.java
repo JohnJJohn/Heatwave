65ecfa44bde14b1f0261b115d7c0ded21ec3e96f
Murphy Ibbs <Murphyibbs@gmail.com>
2024-11-22 14:47:29 +0000

    Initial commit

---
+++ src/main/java/com/heatwave/HeatWaveClient.java
@@ -0,0 +1,10 @@
+package com.heatwave;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HeatWaveClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/heatwave/HeatWaveDataGenerator.java
@@ -0,0 +1,11 @@
+package com.heatwave;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HeatWaveDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
